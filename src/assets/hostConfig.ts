1c952475f193e5fa1e2d0b1bed3a8d9885f7f237
andrewsemenoff <107771959+andrewsemenoff@users.noreply.github.com>
2023-09-18 13:53:20 +0300

    updated base url

--- src/assets/hostConfig.ts
+++ src/assets/hostConfig.ts
@@ -1,7 +1,7 @@
-export const ACCOUNTING_URL = "http://192.168.169.44/user";
-export const PROBLEMS_URL = "http://192.168.169.44/problem";
-export const COMMENTS_URL = "http://192.168.169.44/comment";
-export const SOLUTIONS_URL = "http://192.168.169.44/solution";
-export const COMMUNITIES_URL = "http://192.168.169.44/community";
+export const ACCOUNTING_URL = "http://192.168.169.44:8080/user";
+export const PROBLEMS_URL = "http://192.168.169.44:8081/problem";
+export const COMMENTS_URL = "http://192.168.169.44:8082/comment";
+export const SOLUTIONS_URL = "http://192.168.169.44:8083/solution";
+export const COMMUNITIES_URL = "http://192.168.169.44:8084/community";
 
 // 192.168.169.44
