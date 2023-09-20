2f58632b2081b2cc268e3dbdf5433067e3aaee50
andrewsemenoff <107771959+andrewsemenoff@users.noreply.github.com>
2023-09-20 10:54:33 +0300

    changed base url from localhost to https://192.168.169.44

--- src/assets/hostConfig.ts
+++ src/assets/hostConfig.ts
@@ -1,13 +1,13 @@
-// export const ACCOUNTING_URL = "https://192.168.169.44:8080/user";
-// export const PROBLEMS_URL = "https://192.168.169.44:8081/problem";
-// export const COMMENTS_URL = "https://192.168.169.44:8082/comment";
-// export const SOLUTIONS_URL = "https://192.168.169.44:8083/solution";
-// export const COMMUNITIES_URL = "https://192.168.169.44:8084/community";
+export const ACCOUNTING_URL = "https://192.168.169.44:8080/user";
+export const PROBLEMS_URL = "https://192.168.169.44:8081/problem";
+export const COMMENTS_URL = "https://192.168.169.44:8082/comment";
+export const SOLUTIONS_URL = "https://192.168.169.44:8083/solution";
+export const COMMUNITIES_URL = "https://192.168.169.44:8084/community";
 
 // 192.168.169.44
 
-export const ACCOUNTING_URL = "https://localhost:8080/user";
-export const PROBLEMS_URL = "https://localhost:8081/problem";
-export const COMMENTS_URL = "https://localhost:8082/comment";
-export const SOLUTIONS_URL = "https://localhost:8083/solution";
-export const COMMUNITIES_URL = "https://localhost:8084/community";
+// export const ACCOUNTING_URL = "https://localhost:8080/user";
+// export const PROBLEMS_URL = "https://localhost:8081/problem";
+// export const COMMENTS_URL = "https://localhost:8082/comment";
+// export const SOLUTIONS_URL = "https://localhost:8083/solution";
+// export const COMMUNITIES_URL = "https://localhost:8084/community";
